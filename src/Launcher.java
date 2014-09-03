da6efac9e1da8fab3aea72d571f9c121f3af669d
Jimmy <jimmec@gmail.com>
2014-09-02 18:03:21 -0700

    initial commit. added permuteString solution

---
+++ src/Launcher.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 02/09/14
+ * Time: 5:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/PermuteString.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 02/09/14
+ * Time: 5:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PermuteString {
+    public static String[] permute(String str) {
+        // Base case
+        if (str.length() < 2) {
+            return new String[] {str};
+        }
+
+        String firstChar = str.substring(0,1);
+        String[] perms = permute(str.substring(1));
+        List<String> fullPerms = new ArrayList<String>();
+        for (String perm: perms) {
+            // Insert the remaining charater at each possible position
+            for (int i = 0; i < str.length(); i++) {
+                StringBuilder fullPerm = new StringBuilder(perm);
+                fullPerm.insert(i,firstChar);
+                fullPerms.add(fullPerm.toString());
+            }
+        }
+        String[] result = new String[str.length() * fullPerms.size()];
+        return fullPerms.toArray(result);
+    }
+}
